3907f27cd1993845d24f1f390ec95ad833656865
jacare-camabox <ramonlindo2311@gmail.com>
2022-04-14 01:43:37 -0300

    new routes

--- index.js
+++ index.js
@@ -17,35 +17,39 @@ app.get('/', (req, res) => {
     res.render('index.ejs')
 });
 
-const bases =[
-{
-    id: 1,
-    nome: "floresta petrea",
-    descricao: "a base mais facil",
-    preço: "50.00"
-},
-{
-    id: 2,
-    nome: "Plankerton",
-    descricao: "a base facil",
-    preço: "50.00"
-},
-{
-    id: 3,
-    nome: "Canny Valley",
-    descricao: "a base media",
-    preço: "50.00"
-},
-{
-    id: 4,
-    nome: "Twine Peaks",
-    descricao: "a base mais dificil ",
-    preço: "50.00"
-},
 
-]
+
+app.get('/floresta', (req, res) => {
+    res.render('floresta.ejs')
+});
+
+app.get('/Plankerton', (req, res) => {
+    res.render('plankerton.ejs')
+});
+
+app.get('/CannyValley', (req, res) => {
+    res.render('CannyValley.ejs')
+});
+
+app.get('/TwinePeaks', (req, res) => {
+    res.render('TwinePeaks.ejs')
+});
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 app.listen(port, ()=> {
     console.log(`estou na porta ${port}` )
 });
+
+
